ca0ac794347cc6f4d456a1c5230791b241513e89
Abhishek Yadav <abhishekaug2002@gmail.com>
2022-11-02 00:30:55 +0530

    route update

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Message from "./components/MessageVolume/message";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
           <Router>
               <Routes>
-                  <Route path="/" element={<Navbar/>} />
-                  <Route path="/message" element={<Message/>} />
-                  <Route path="/home" element={<Home/>} />
+                  <Route path="/" element={<Home/>} />
 
               </Routes>
           </Router>
